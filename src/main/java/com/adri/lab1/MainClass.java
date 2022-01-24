0523ec0de750c66db47a99fe76ae8a510c46352a
Cristian <Cristian@DESKTOP-MQ7EUCR.attlocal.net>
2022-01-24 13:09:36 -0500

    Testing

---
+++ src/main/java/com/adri/lab1/MainClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.adri.lab1;
+
+/**
+ * @author Cristian
+ *
+ */
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
